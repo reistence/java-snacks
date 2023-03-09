6f25f9813c0c3a34a498099d8e2b57f209a03317
reistence <gvlamberti@gmail.com>
2023-03-09 15:02:00 +0100

    snacks4 all arrays and nums printed

--- src/snacks/Snack4.java
+++ src/snacks/Snack4.java
@@ -1,5 +1,6 @@
 package snacks;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Snack4 {
@@ -8,11 +9,14 @@ public class Snack4 {
     elementi random finchè il numero di elementi presenti nell’array che ne ha di meno + quelli
     mostrati a video non è uguale al numero di elementi presenti nell’array che ne ha di più  */
     public static void main(String[] args) {
-        int[] small = new int[5];
-        int[] large = new int[10];
+        int[] small = {23, 45, 68, 77, 69};
+        int[] large = {99, 89, 232, 44, 33, 13, 15, 7, 9, 10};
         Random random = new Random();
         for (int i = 0; i < (large.length - small.length); i++) {
-            System.out.println(random.nextInt(100));
+            System.out.print(random.nextInt(100) + " ,");
+
         }
+            System.out.println(Arrays.toString(small));
+            System.out.println(Arrays.toString(large));
     }
 }
